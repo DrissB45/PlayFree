1d74b817e0e66edde3fbe753a41e8c69df6fd964
DRISS BOUMEDIENE <driss308@hotmail.fr>
2023-02-01 21:33:13 +0100

    all games catalogue fait

--- src/Controller/CatalogueController.php
+++ src/Controller/CatalogueController.php
@@ -24,6 +24,29 @@ class CatalogueController extends AbstractController
     {
         $response = $this->client->request(
             'GET',
+            'https://api.rawg.io/api/games?key=' . self::API_KEY
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $games = $response->toArray()['results'];
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+       /*  var_dump($games); exit(); */
+
+        return $this->render('catalogue/index.html.twig', [
+            'games' => $games
+        ]);
+    }
+
+    #[Route('/consoles', name: 'consoles')]
+    public function consoles(): Response
+    {
+        $response = $this->client->request(
+            'GET',
             'https://api.rawg.io/api/platforms?key=' . self::API_KEY
         );
 
