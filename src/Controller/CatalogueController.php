5c13712b6645145da501e9f5c5266b7cf471d2bd
DRISS BOUMEDIENE <driss308@hotmail.fr>
2023-02-02 09:17:04 +0100

    service done

--- src/Controller/CatalogueController.php
+++ src/Controller/CatalogueController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use GMP;
 use App\Entity\Game;
 use App\Repository\GameRepository;
+use App\Service\InsertInDatabase;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -13,42 +14,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/catalogue', name: 'catalogue_')]
 class CatalogueController extends AbstractController
 {
-    private const API_KEY = 'b4c2c261dfdf4eb9a4ee35f459bc938b';
-
-    private $client;
-
-    public function __construct(HttpClientInterface $client)
+    public function __construct(private InsertInDatabase $insertInDatabase)
     {
-        $this->client = $client;
+        $this->insertInDatabase = $insertInDatabase;
     }
 
     #[Route('/', name: 'index')]
-    public function index(GameRepository $gameRepository): Response
+    public function index(): Response
     {
-        $response = $this->client->request(
-            'GET',
-            'https://api.rawg.io/api/games?key=' . self::API_KEY
-        );
-
-        $statusCode = $response->getStatusCode();
-        // $statusCode = 200
-        $contentType = $response->getHeaders()['content-type'][0];
-        // $contentType = 'application/json'
-        $content = $response->getContent();
-        // $content = '{"id":521583, "name":"symfony-docs", ...}'
-        $dataGames = $response->toArray()['results'];
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
-        
-        foreach ($dataGames as $dataGame) {
-            $game = new Game();
-            $game->setTitle($dataGame['name']);
-            $game->setImage($dataGame['background_image']);
-            $game->setGenre($dataGame['genres'][0]['name']);
-            $gameRepository->save($game, true);
-        }
-        
         return $this->render('catalogue/index.html.twig', [
-            'games' => $gameRepository->findAll()
+            'games' => $this->insertInDatabase->recordGames()
         ]);
     }
     
@@ -60,7 +35,7 @@ class CatalogueController extends AbstractController
         ]);
     }
 
-    #[Route('/consoles', name: 'consoles')]
+    /* #[Route('/consoles', name: 'consoles')]
     public function consoles(): Response
     {
         $response = $this->client->request(
@@ -78,9 +53,8 @@ class CatalogueController extends AbstractController
         // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
         /* var_dump($content); exit(); */
 
-        return $this->render('catalogue/consoles.html.twig', [
+        /* return $this->render('catalogue/consoles.html.twig', [
             'content' => $content
-        ]);
+        ]); */
     }
 
-}
---
+++ src/Service/InsertInDatabase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Game;
+use App\Repository\GameRepository;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class InsertInDatabase
+{
+    private const API_KEY = 'b4c2c261dfdf4eb9a4ee35f459bc938b';
+
+    private GameRepository $gameRepository;
+
+    public function __construct(private HttpClientInterface $client, GameRepository $gameRepository)
+    {
+        $this->client = $client;
+        $this->gameRepository = $gameRepository;
+    }
+
+    public function recordGames()
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://api.rawg.io/api/games?key=' . self::API_KEY
+        );
+
+        $dataGames = $response->toArray()['results'];
+
+        foreach ($dataGames as $dataGame) {
+            $game = new Game();
+            $game->setTitle($dataGame['name']);
+            $game->setImage($dataGame['background_image']);
+            $game->setGenre($dataGame['genres'][0]['name']);
+            $this->gameRepository->save($game, true);
+        }
+        
+        return $this->gameRepository->findAll();
+    }
+}
